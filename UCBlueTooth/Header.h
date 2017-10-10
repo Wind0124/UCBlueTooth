460004839429f1cb92e2faef7e4694648b5c2ceb
Wind <wind@WinddeMacBook-Air.local>
2017-10-10 15:05:14 +0800

    create

--- UCBlueTooth/Header.h
+++ UCBlueTooth/Header.h
@@ -10,4 +10,25 @@
 #define Header_h
 
 
+
+
+
+
+
+
+
+
+// Debug时打印Log，发布时禁止打印Log
+//http://blog.csdn.net/win_mary/article/details/52813991
+#ifdef DEBUG
+#define NSLog(...)  NSLog(__VA_ARGS__)
+#else
+#define NSLog(...)
+#endif
+
+// 设置屏幕宽度 高度
+#define DEVICE_HEIGHT    [[UIScreen mainScreen] bounds].size.height
+#define DEVICE_WIDTH     [[UIScreen mainScreen] bounds].size.width
+
 #endif /* Header_h */
+
